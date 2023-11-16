8ece6c300f70559f6fc01f9ffe7deb33af544929
Maciej Trzaskacz <maciejtrzask@student.agh.edu.pl>
2023-11-16 16:32:26 +0100

    getPracownik

--- app/src/main/java/com/example/worktracker/Database.java
+++ app/src/main/java/com/example/worktracker/Database.java
@@ -1,6 +1,8 @@
 package com.example.worktracker;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Database {
@@ -8,8 +10,10 @@ public class Database {
     public static Pracownik getEmployee(String id) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference ref = db.collection("pracownicy").document(id);
-        ref.get();
-        return null;
+        Task<DocumentSnapshot> task = ref.get();
+        while(!task.isComplete()) {}
+        DocumentSnapshot p = task.getResult();
+        return p.toObject(Pracownik.class);
     }
 
     //Adds employee to database and returns a reference to the new document (use ref.getID to get employee id from it)
