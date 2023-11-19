c01398bd879224018d9db845d00e65af5d696ff6
ZakatorDerg <adamtokarz.kol@gmail.com>
2023-11-19 22:06:06 +0100

    przesuwany ekran ciąg dalszy I guess

--- app/src/main/java/com/example/worktracker/WTrakcie.java
+++ app/src/main/java/com/example/worktracker/WTrakcie.java
@@ -2,13 +2,32 @@ package com.example.worktracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WTrakcie extends AppCompatActivity {
+    private Button button_back_zad2;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wtrakcie);
+
+        button_back_zad2 = (Button) findViewById(R.id.button_back_zad2);
+        button_back_zad2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openZadania();
+            }
+        });
+    }
+
+    public void openZadania() {
+        Intent intent = new Intent(this, Zadania.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/worktracker/Wykonane.java
+++ app/src/main/java/com/example/worktracker/Wykonane.java
@@ -2,13 +2,32 @@ package com.example.worktracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Wykonane extends AppCompatActivity {
+    private Button button_back_zad3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wykonane);
+
+        button_back_zad3 = (Button) findViewById(R.id.button_back_zad3);
+        button_back_zad3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openZadania();
+            }
+        });
     }
+
+    public void openZadania() {
+        Intent intent = new Intent(this, Zadania.class);
+        startActivity(intent);
+    }
+
+
 }
