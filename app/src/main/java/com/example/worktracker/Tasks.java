b938120c4a588aa512c803d2c920104d33ddadb2
Artwojciech <artwojciech@interia.pl>
2024-01-11 22:59:24 +0100

    opisy

--- app/src/main/java/com/example/worktracker/Tasks.java
+++ app/src/main/java/com/example/worktracker/Tasks.java
@@ -2,6 +2,10 @@ package com.example.worktracker;
 
 import com.google.firebase.firestore.Exclude;
 
+/**
+ * Klasa reprezentuje zadanie danego pracownika. Przechowuje dane o tym zadaniu. Zawiera gettery i
+ * settery potrzebne do pobierania i modyfikowania zmiennych
+ */
 public class Tasks {
     private String id;
     private String nazwa;
@@ -41,6 +45,11 @@ public class Tasks {
         return this;
     }
 
+    /**
+     * Pozwala na zmiane statusu danego zadania
+     * @param status status na ktory chcemy zmienic aktualny status zadania
+     * @return zadanie ktoremu wlasnie zmienilismy status
+     */
     public Tasks changeStatus(String status) {
         if (status.equals("Do zrobienia") || status.equals("W trakcie") || status.equals("Wykonane"))
             this.status = status;
